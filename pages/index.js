56d4f8b5719060bf029933c0c81565e9ba08e37e
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-08-01 11:19:44 -0500

    :pencil: completion of lesson 1 & 2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="container">
       <Head>
@@ -10,7 +11,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -205,5 +209,7 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
