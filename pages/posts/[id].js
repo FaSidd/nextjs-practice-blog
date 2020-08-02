9a0c914aadda96a117298e5541ba7182caa97223
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-08-02 16:11:36 -0500

    :twisted_rightwards_arrows:  NOW completed Lesson 5: Dynamic Routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,23 @@
 import Layout from '../../components/layout';
+import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+
+import utilStyles from '../../styles/utils.module.css';
 
 const Post = ({ postData }) => {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 };
@@ -22,7 +31,7 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async ({ params }) => {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
